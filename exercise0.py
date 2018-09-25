4eaf0c4a30d84ac6ad8ad37f505cb269d4b1fe8e
REEEYZ <guyuzhe2009@aliyun.com>
2018-09-24 21:46:41 -0500

    exercise0

---
+++ exercise0.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+for i in range(6):
+    print i,
+
+print ""
+
+for i in range(7):
+    print i + 2,
+
+print ""
+
+for i in range(10):
+    print i,
+    
+print ""
+    
+for i in ['one', 'two','three','four']:
+    if(i == 'four'):
+        print i,
+    else:
+        print i + " and a",
+
+
+print ""
+
+for i in range(1, 6):
+    for j in range(i + 1):
+        print j,
+    print ""
+    
+    
+    
+    
+    
+    
+    
+    
