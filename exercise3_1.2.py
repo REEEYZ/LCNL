efcba92dc75befa5849154bdd844f8cd538f5b5d
REEEYZ <guyuzhe2009@aliyun.com>
2018-10-01 22:00:36 -0500

    exercise3 1.2

---
+++ exercise3_1.2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import random
+
+
+def main():
+    
+    test = []
+    mask = ["masked", "nonmasked", "masked"]
+    direction = ["left", "right"]
+    for i in range(5):
+        for j in range(6):
+            num = i + 1
+            maskChoice = random.choice(mask)
+            direc = random.choice(direction)
+            test.append([num, maskChoice, direc])
+            
+            
+    for i in range(len(test)):
+        for j in range(len(test[i])):
+            print(test[i][j]),
+        print ""
+        
+        
+        
+    
+    
+if __name__ == "__main__":
+    main()
+
